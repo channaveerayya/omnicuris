5342599de9ca14c5304ffad74173cac411deb566
channaveerayya <channucs0009@gmail.com>
2020-05-17 20:51:35 +0530

    Updates

---
+++ precache-manifest.70e9e94da53e027af7cd18f9e907161d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4decaef7bdb65ec1cb992e54dbe8fd",
+    "url": "/omnicuris/index.html"
+  },
+  {
+    "revision": "0f0f99c012316b266412",
+    "url": "/omnicuris/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b69d258dec2d3e5a755",
+    "url": "/omnicuris/static/css/main.9d7ff2bf.chunk.css"
+  },
+  {
+    "revision": "0f0f99c012316b266412",
+    "url": "/omnicuris/static/js/2.bf239665.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/omnicuris/static/js/2.bf239665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b69d258dec2d3e5a755",
+    "url": "/omnicuris/static/js/main.ca8e157f.chunk.js"
+  },
+  {
+    "revision": "83448f293b30a22c1adb",
+    "url": "/omnicuris/static/js/runtime-main.d246c7ed.js"
+  },
+  {
+    "revision": "71ce71bfa276cedb93eb9496b1d6554d",
+    "url": "/omnicuris/static/media/facebook.71ce71bf.png"
+  },
+  {
+    "revision": "0f72b24b7b8c9b5c51ff3e2360a1c4ac",
+    "url": "/omnicuris/static/media/whatsapp.0f72b24b.png"
+  },
+  {
+    "revision": "d9eff1e55e897f1fcb885ef415f95546",
+    "url": "/omnicuris/static/media/youtub.d9eff1e5.png"
+  }
+]);
